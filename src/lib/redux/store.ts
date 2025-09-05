8863c19f46dd5dec4f14a6b5ec63aefc8542db0f
RuetGG <ruthgashahun1@gmail.com>
2025-09-05 16:51:56 +0300

    Added Table feature, works works with mock data

--- src/lib/redux/store.ts
+++ src/lib/redux/store.ts
@@ -1,14 +1,17 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { fixtureApi } from '../api/fixtureApi';
+import { tableApi } from '../api/tableApi';
 
 export const store = configureStore({
   reducer: {
     [fixtureApi.reducerPath]: fixtureApi.reducer,
+    [tableApi.reducerPath]: tableApi.reducer,
     
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(
       fixtureApi.middleware,
+      tableApi.middleware
     ),
 })
 
